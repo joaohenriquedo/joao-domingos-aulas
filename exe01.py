ffdc859b6faccb38e3009fe330cf4ae0aab9a49f
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-06 21:26:35 -0300

    Update exe01.py
    alteraçao

--- exe01.py
+++ exe01.py
@@ -1,9 +1,9 @@
  x = 34 - 23 #um comentario
  y = "Hello" #outro comentario
-z = 3.45
-if z == 3.45 or y == "Hello":
+ z = 3.45
+ if z == 3.45 or y== "Hello":
     x = x + 1
-    y = y + " World" #cancatenação de strings
+    y = y + " World" #concatenação de strings
 print(x)
 print(y)
 
