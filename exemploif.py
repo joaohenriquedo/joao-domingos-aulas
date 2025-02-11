0405ed4eb05d75847382a89e66ef6d03b45b9620
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-11 20:08:06 -0300

    Update exemploif.py

--- exemploif.py
+++ exemploif.py
@@ -1,2 +1,8 @@
-num =
-if 
+num = int(input("digite um numero"))
+if num>10:
+    print("o numero é maior que 10")
+else:
+    if num<10:
+     print("o numero é menor que 10")
+    else:
+     print("o numero é 10")
