69d0a670cb3b92a94055e87a99ea719d3e41c226
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-19 19:25:05 -0300

    Create exemplo_tupla.py

---
+++ exemplo_tupla.py
@@ -0,0 +1,7 @@
+vendas = ("lira", "20/3/2018", "299", 2000, "estagiario")
+nome, data_contratação, data_nascimento, salario, cargo = vendas
+print(nome)
+print(data_contratação)
+print(data_nascimento)
+print(salario)
+print(cargo)
