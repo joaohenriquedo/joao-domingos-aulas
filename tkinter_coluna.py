a4a468b9f94cf9211bdc333d2246bb815ac8f94d
joaohenriquedo <joaohenriquedomingos2008@gmail.com>
2025-02-25 22:13:40 -0300

    777

--- tkinter_coluna.py
+++ tkinter_coluna.py
@@ -4,7 +4,7 @@ i.title("tabuada feita por João Henrique")
 i.geometry('980x720+250+30')
 
 
-lb1 = Label(i,text="login",bg="yellow")
+'''lb1 = Label(i,text="login",bg="yellow")
 #componente .grid serve tambem para posicionar utilizando indicativo de linha(row) e coluna(collumn)
 lb1.grid(row=2,column=1)
 
@@ -18,6 +18,23 @@ ed2 = Entry(i)
 ed2.grid(row=4, column=4)
 
 bt1 = Button(i,text="Login")
-bt1.grid(row=3,column=2)
+bt1.grid(row=3,column=2)'''
+
+# O CODIGO ABAIXO FAZ CORREÇÃO DA SPOSIÇÕES DAS LABLES, CAIXA DE TEXTO E BOTAO
+lb1 = Label(i,text="login",bg="yellow")
+lb1.grid(row=2,column=1)
+
+lb2 = Label(i,text="SENHA", bg="blue")
+lb2.grid(row=2, column=1)
+
+ed1 = Entry(i)
+ed1.grid(row=1, column=2)
+
+ed2 = Entry(i)
+ed2.grid(row=2, column=2)
+
+bt1 = Button(i,text="Login")
+bt1.grid(row=2,column=2)
+
 
 i.mainloop()
